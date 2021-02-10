a769fbc7cbbf92149009be6e17c6208e65bb760a
wangyi <wangyi@megvii.com>
2021-02-10 16:33:21 +0800

    feat: 🎸 complete builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -3,7 +3,7 @@ use quote::{format_ident, quote, quote_spanned};
 use syn::spanned::Spanned;
 use syn::{parse_macro_input, Data, DeriveInput, Field, Fields, Type};
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident;
@@ -11,7 +11,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let members = foreach_members(&input.data, |f| {
         let name = format_ident!("builder_member_{}", f.ident.as_ref().unwrap());
         let ty = &f.ty;
-        quote_spanned!(f.span()=> #name : Option<#ty>,)
+        quote_spanned!(f.span()=> #name : std::option::Option<#ty>,)
     });
     let setters = foreach_members(&input.data, |f| {
         let name = &f.ident;
@@ -20,16 +20,73 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
             let ty = inner_ty;
             quote_spanned! {f.span()=>
                 pub fn #name (&mut self, v: #ty) -> &mut Self {
-                    self.#member_name = Some(Some(v));
+                    self.#member_name = std::option::Option::Some(std::option::Option::Some(v));
                     self
                 }
             }
         } else {
             let ty = &f.ty;
-            quote_spanned! {f.span()=>
-                pub fn #name (&mut self, v: #ty) -> &mut Self {
-                    self.#member_name = Some(v);
-                    self
+            let (repeat, expanded) = if let Some(attr) = f.attrs.first() {
+                if attr.path.segments[0].ident == "builder" {
+                    let each = if let Ok(syn::Meta::List(ref nvs)) = attr.parse_meta() {
+                        if nvs.path.get_ident().unwrap() != "builder" {
+                            return syn::Error::new_spanned(
+                                nvs,
+                                "expected `builder(each = \"...\")`",
+                            )
+                            .to_compile_error();
+                        }
+                        if let Some(syn::NestedMeta::Meta(syn::Meta::NameValue(kv))) =
+                            nvs.nested.first()
+                        {
+                            if kv.path.get_ident().unwrap() != "each" {
+                                return syn::Error::new_spanned(
+                                    nvs,
+                                    "expected `builder(each = \"...\")`",
+                                )
+                                .to_compile_error();
+                            }
+                            match &kv.lit {
+                                syn::Lit::Str(each) => {
+                                    syn::Ident::new(each.value().as_str(), each.span())
+                                }
+                                _ => unreachable!(),
+                            }
+                        } else {
+                            unreachable!()
+                        }
+                    } else {
+                        unreachable!()
+                    };
+                    let inner_ty = ty_inner_type("Vec", ty).unwrap();
+                    (
+                        name.as_ref().unwrap() == &each,
+                        quote_spanned! {f.span()=>
+                            pub fn #each (&mut self, #each: #inner_ty) -> &mut Self {
+                                if let std::option::Option::Some(ref mut container) = self.#member_name {
+                                    container.push(#each);
+                                } else {
+                                    self.#member_name = std::option::Option::Some(vec![#each]);
+                                }
+                                self
+                            }
+                        },
+                    )
+                } else {
+                    (false, quote!())
+                }
+            } else {
+                (false, quote!())
+            };
+            if repeat {
+                quote_spanned!(f.span()=> #expanded)
+            } else {
+                quote_spanned! {f.span()=>
+                    pub fn #name (&mut self, v: #ty) -> &mut Self {
+                        self.#member_name = std::option::Option::Some(v);
+                        self
+                    }
+                    #expanded
                 }
             }
         }
@@ -37,21 +94,21 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let builder = foreach_members(&input.data, |f| {
         let name = &f.ident;
         let member_name = format_ident!("builder_member_{}", f.ident.as_ref().unwrap());
-        quote_spanned!(f.span()=> #name : self.#member_name.clone().unwrap_or(Default::default()),)
+        quote_spanned!(f.span()=> #name : self.#member_name.clone().unwrap_or(std::default::Default::default()),)
     });
     let expanded = quote! {
         impl #name {
             pub fn builder() -> #builder_name {
-                Default::default()
+                std::default::Default::default()
             }
         }
-        #[derive(Default)]
+        #[derive(std::default::Default)]
         pub struct #builder_name {
             #members
         }
         impl #builder_name {
             #setters
-            pub fn build(&self) -> Result<#name, &'static str> {
+            pub fn build(&self) -> std::result::Result<#name, &'static str> {
                 Ok(#name {
                     #builder
                 })
